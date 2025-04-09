561b52bb5fcd595cea0c87e7a99067558264c5d8
Volker Simonis <volker.simonis@gmail.com>
2025-04-09 19:19:15 +0200

    Add option to RsaTest.java to load ACCP

--- src/main/java/io/simonis/nativeimage/test/RsaTest.java
+++ src/main/java/io/simonis/nativeimage/test/RsaTest.java
@@ -7,21 +7,35 @@ import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
+import java.security.Provider;
 import java.security.PublicKey;
+import java.security.Security;
 import java.util.Base64;
+import java.util.ServiceLoader;
 
 public class RsaTest {
     public static void main(String[] args) throws Exception {
+        Provider accp = null;
+        ServiceLoader<Provider> pl = ServiceLoader.load(Provider.class);
+        for (Provider p : pl) {
+            System.out.println(p.getName());
+            if ("AmazonCorrettoCryptoProvider".equals(p.getName())) {
+                accp = p;
+            }
+        }
+        String message = args.length == 0 ? "RSA encryption/decryption test" : args[0];
+        if (message.toLowerCase().contains("accp") && accp != null) {
+            Security.insertProviderAt(accp, 1);
+        }
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("<RETURN>");
         in.readLine();
         KeyPairGenerator rsaGen = KeyPairGenerator.getInstance("RSA");
         System.out.println("RSA KeyPairGenerator provider: " + rsaGen.getProvider().getName());
-        rsaGen.initialize(1024);
+        rsaGen.initialize(2048);
         KeyPair rsa = rsaGen.generateKeyPair();
         PrivateKey rsaPrivate = rsa.getPrivate();
         PublicKey rsaPublic = rsa.getPublic();
-        String message = args.length == 0 ? "RSA encryption/decryption test" : args[0];
         Cipher rsaEncryptCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
         System.out.println("RSA Cipher provider: " + rsaEncryptCipher.getProvider().getName());
         rsaEncryptCipher.init(Cipher.ENCRYPT_MODE, rsaPublic);
