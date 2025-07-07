d57cb836fd29c96dae00fd573d9d8f5d1f9646f4
Volker Simonis <volker.simonis@gmail.com>
2025-07-07 18:28:59 +0000

    Added basic GraalPython example   - build with: mvn -f pompy.xml clean package   - run with: java -cp target-python/python-deps/*:target-python/graal-python-test-1.0-SNAPSHOT.jar io.simonis.graalpython.test.PythonEmbedding

---
+++ src/main/java/io/simonis/graalpython/test/PythonEmbedding.java
@@ -0,0 +1,40 @@
+package io.simonis.graalpython.test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Engine;
+import org.graalvm.polyglot.Value;
+
+public class PythonEmbedding {
+  public static void main(String[] args) throws Exception {
+    Engine engine = Engine.newBuilder("python").build();
+    System.out.println("Engine module: " + engine.getClass().getModule());
+    System.out.println("==================== Engine Options ====================");
+    engine.getOptions().forEach(od -> System.out.println(od.getName() + " : \n\t" + od.getHelp() + "\n\t" + od.getKey().getDefaultValue()));
+    System.out.println("==================== Python Options ====================");
+    engine.getLanguages().get("python").getOptions().forEach(od -> System.out.println(od.getName() + " : \n\t" + od.getHelp() + "\n\t" + od.getKey().getDefaultValue()));
+    //engine.getLanguages().get("python").getSourceOptions().forEach(od -> System.out.println(od));
+    try (Context context = Context.newBuilder("python")
+         .engine(engine)
+         .option("python.PythonHome", "/tmp")
+         .allowAllAccess(true)
+         .build()) {
+      System.out.println("Context: " + context);
+      Value function = context.eval("python", "print('Hello from GraalPython');\nlambda x : x+1");
+      assert function.canExecute();
+      int x = function.execute(41).asInt();
+      assert x == 42;
+      while (true) {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line = in.readLine();
+        try {
+          System.out.println(context.eval("python", line));
+        } catch (Exception e) {
+          System.out.println(e);
+        }
+      }
+    }
+  }
+}
